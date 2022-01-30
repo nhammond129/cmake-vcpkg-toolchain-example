0668b7f7f27e7f9e7369a5977bbf802af347bbe2
Nicholas Hammond <nhammond129@gmail.com>
2022-01-29 23:30:47 -0600

    setup bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-# cwd=`pwd`
-# git submodule update --init
-# cd $cwd/build
-# cmake ..
-# cmake --build .
-# cd $cwd
-# echo $cwd/build/debug/test
+cwd=`pwd`
+
+prepare() {
+	cd $cwd
+	git submodule update --init
+	mkdir -p $cwd/build
+}
+
+build() {
+	cd $cwd/build
+	cmake ..
+	cmake --build .
+	cd $cwd
+}
+
+main() {
+	prepare
+	build
+
+	echo -e "\n\nOutput:\n"
+	find $cwd/build -executable -type f ! -path "$cwd/build/CMakeFiles/*"
+}
+
+main

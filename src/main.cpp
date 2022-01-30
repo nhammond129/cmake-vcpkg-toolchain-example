31713e05d3167318166b87e71d66dbaeb7fb764a
Nicholas Hammond <nhammond129@gmail.com>
2022-01-30 01:09:31 -0600

    sdl-using example code; x11 feature for sdl2 on linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,29 @@
+#include <SDL2/SDL.h>
 #include <iostream>
 
+#define WIDTH  640
+#define HEIGHT 480
+#define SDL_FLAGS SDL_WINDOW_SHOWN
+
+
 int main() {
-	std::cout << "hello world" << std::endl;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	if (SDL_Init(SDL_INIT_EVERYTHING)) return 1;
+	if (SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, SDL_FLAGS, &window, &renderer)) return 1;
+
+	while(true) {
+		SDL_SetRenderDrawColor(renderer, 20, 20, 20, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(renderer);
+
+		SDL_Rect aabb { 25, 25, 69, 420 };
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
+		SDL_RenderFillRect(renderer, &aabb);
+
+		SDL_RenderPresent(renderer);
+	}
+
+	if (renderer) SDL_DestroyRenderer(renderer);
+	if (window) SDL_DestroyWindow(window);
+	SDL_Quit();
 }
